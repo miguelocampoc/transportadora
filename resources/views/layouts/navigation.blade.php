d3faf1acdaa8135079a9225d8d8a47f18d210604
Mateo Fonseca <mateofon78@hotmail.com>
2022-11-06 16:22:51 -0500

    rutas de ley

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,20 +12,20 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('usuarios')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('usuarios')" :active="request()->routeIs('usuarios')">
                         Usuarios
                     </x-nav-link>
                     <x-nav-link :href="route('mapa')" :active="request()->routeIs('mapa')">
                         Mapa
-                   </x-nav-link>
-                    <x-nav-link :href="route('conductores')" :active="request()->routeIs('dashboard')">
+                    </x-nav-link>
+                    <x-nav-link :href="route('conductores')" :active="request()->routeIs('conductores')">
                         Conductores
                     </x-nav-link>
-                    <x-nav-link :href="route('vehiculos')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('vehiculos')" :active="request()->routeIs('vehiculos')">
                         Vehiculos
                     </x-nav-link>
-                    <x-nav-link :href="route('ley')" :active="request()->routeIs('dashboard')">
-                        Ley 
+                    <x-nav-link :href="route('ley')" :active="request()->routeIs('ley')">
+                        Ley
                     </x-nav-link>
                 </div>
             </div>
@@ -74,14 +74,20 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('usuarios')" :active="request()->routeIs('usuarios')">
                 Usuarios
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('mapa')" :active="request()->routeIs('map')">
+                Mapa
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('conductores')" :active="request()->routeIs('conductores')">
                 Conductores
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('vehiculos')" :active="request()->routeIs('vehiculos')">
                 Vehiculos
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('ley')" :active="request()->routeIs('ley')">
+                Ley
             </x-responsive-nav-link>
         </div>
 

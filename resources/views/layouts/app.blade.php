910b26d4273de7332cf232dea4e2518d2bb57f22
Miguel Ocampo <miguelocampoc@gmail.com>
2022-11-10 20:17:08 -0500

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,14 +15,20 @@
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <script src="https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.js"></script>
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
 
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.css"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />  
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href='https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css' rel='stylesheet' />
+        <link href="{{asset('AppMapbox\MapBox\Mapa Directions\mapbox-gl-directions-master\src\mapbox-gl-directions.css')}}" rel="stylesheet">
+        
     </head>
     <body class="font-sans antialiased">
+    <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.js"></script>
+    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css" type="text/css">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
@@ -44,6 +50,8 @@
         <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+        <script src='https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.js'></script>
 
+        <script src="{{asset('AppMapbox\MapBox\mapa.js')}}"></script>
     </body>
 </html>
--- resources/views/mapa/index.blade.php
+++ resources/views/mapa/index.blade.php
@@ -8,9 +8,113 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                   
+                    <p> Listado de Bodegas </p>
+                    <div class="row">
+                        <div class="col-xl-4">
+                            <select class="form-control" name="bodegaA" id="bodegaA">
+                                <option value="-73.85614,7.06734"> Barrancabermeja</option>
+                                <option value="-74.04371,4.74584"> Bogota</option>
+                                <option value="-75.56755,6.29853"> Medellin</option>
+                                <option value="-73.12989,7.15175"> Bucaramanga</option>
+                                <option value="-74.81386,11.01663"> Barranquilla</option>
+
+                            </select>
+
+                        </div>
+                        <div class="col-xl-4">
+
+                            <select class="form-control" name="bodegaB" id="bodegaB">
+                                <option value="-73.85614,7.06734"> Barrancabermeja</option>
+                                <option value="-74.04371,4.74584"> Bogota</option>
+                                <option value="-75.56755,6.29853"> Medellin</option>
+                                <option value="-73.12989,7.15175"> Bucaramanga</option>
+                                <option value="-74.81386,11.01663"> Barranquilla</option>
+
+
+                            </select>
+                        </div> 
+                        <div class="col-xl-4">
+                            <button class="btn btn-primary" onclick="marcarRuta()">Marcar ruta</button>
+
+                        </div>
+
+                    </div>
+                    <br><br>
+                    <div id='map' style='width: 100%; height: 450px;'></div>
+
                 </div>
             </div>
         </div>
     </div>
+    <script>
+        // TO MAKE THE MAP APPEAR YOU MUST
+        // ADD YOUR ACCESS TOKEN FROM
+        // https://account.mapbox.com
+        const access_token = "pk.eyJ1IjoibWp2YWxlbnp1ZWxhIiwiYSI6ImNrb2Fmdm9zZDBpM28ybnFtYTQ2Z2MwMnYifQ.ZY3jTw0-6tjUSOOJXJHsdw";
+        mapboxgl.accessToken = 'pk.eyJ1IjoibWp2YWxlbnp1ZWxhIiwiYSI6ImNrb2Fmdm9zZDBpM28ybnFtYTQ2Z2MwMnYifQ.ZY3jTw0-6tjUSOOJXJHsdw';
+        const map = new mapboxgl.Map({
+            container: 'map',
+            // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-73.85150, 7.05723],
+            zoom: 13
+        });
+        map.addControl(
+            new MapboxDirections({
+                accessToken: mapboxgl.accessToken
+            }),
+            'top-left'
+        );
+
+
+        const marker = new mapboxgl.Marker();
+
+        function marcarRuta() {
+            if (map.getLayer('route')) {
+                map.removeLayer('route');
+            }
+            if (map.getSource('route')) {
+                map.removeSource('route');
+            }
+            let cA = document.querySelector('#bodegaA').value;
+            let cB = document.querySelector('#bodegaB').value;
+            let url = `https://api.mapbox.com/directions/v5/mapbox/driving/${cA};${cB}?geometries=geojson&access_token=${access_token}`;
+            console.log(url);
+            axios.get(url).then(function(response) {
+
+                let coordinates = response.data.routes[0].geometry.coordinates;
+                console.log(response);
+                CrearRuta(coordinates);
+
+            });
+
+        }
+
+        function CrearRuta(coordinates) {
+            map.addSource('route', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'Feature',
+                    'properties': {},
+                    'geometry': {
+                        'type': 'LineString',
+                        'coordinates': coordinates
+                    }
+                }
+            });
+            map.addLayer({
+                'id': 'route',
+                'type': 'line',
+                'source': 'route',
+                'layout': {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                'paint': {
+                    'line-color': '#888',
+                    'line-width': 8
+                }
+            });
+        }
+    </script>
 </x-app-layout>
